7dcfee4e1b617615ac1b56901bee8b7b6109a87d
Mayank157 <98547771+Mayank157@users.noreply.github.com>
2022-04-17 15:36:07 +0530

    Delete static directory

--- static/js/script2.js
+++
@@ -1,9 +0,0 @@
-
-
-applyOnline=document.querySelector('#apply_online')
-console.log(applyOnline)
-applyOnline.addEventListener('click',()=>
-{
-    onlineShopForm=document.querySelector('#display_form')
-    onlineShopForm.style.display='block'
-})
